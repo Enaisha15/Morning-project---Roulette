b6baf352a1597b3c001b5825beaa4d5e1a385e13
Enaisha <Enaisha15@gmail.com>
2021-10-24 13:35:04 -0400

    Enaisha Roulette

---
+++ config/database.js
@@ -0,0 +1,11 @@
+
+const username  = 'Enaisha15'
+const password  = 'Jasmine123!'
+const dbName    = 'roulette'
+
+module.exports = {
+    'url' : `mongodb+srv://${username}:${password}@cluster0.jrgra.mongodb.net/${dbName}?retryWrites=true&w=majority`
+    // mongodb+srv://Enaisha15:<password>@cluster0.jrgra.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+};
+
+
